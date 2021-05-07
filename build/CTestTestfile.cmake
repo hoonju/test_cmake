a8ce10545f29b7b5bf63ab5d29b2c72b4c55c5d9
Hoonju Chung <hoonju@it-farm.co.kr>
2021-05-08 01:57:02 +0900

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /media/cuda/__Storage/__C++/__tests/test_cmake
+# Build directory: /media/cuda/__Storage/__C++/__tests/test_cmake/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
